68bab849a38bdc6e2082b7336f00183cd65b2c2b
srilaasya <srilaasya1990e@gmail.com>
2024-11-11 18:47:57 +0000

    javascriptexecutor

--- XunitTestProject/WebuyACarTests.cs
+++ XunitTestProject/WebuyACarTests.cs
@@ -22,7 +22,14 @@ public class WebBuyACarTests : IDisposable
 
         // Find the button (update selector based on the actual button)
         IWebElement button = driver.FindElement(By.Id("btn-go")); // Replace "button-id" with actual ID
+        IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)driver;
 
+
+        string milage = (string)jsExecutor.ExecuteScript("return document.getElementById('MileageCheck').value;");
+
+        string Manufacturer = (string)jsExecutor.ExecuteScript("return document.evaluate(\"(//div[contains(text(),'Manufacturer')]/following-sibling::div)[2]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;");
+        ((IJavaScriptExecutor)driver).ExecuteScript("window.scrollBy(0, 500);");
+        ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", button);
         // Click the button
         button.Click();
 
